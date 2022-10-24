9a9b67524197eeee07ffef5b13b77f7346c16449
Jacob Sapoznikow <500074@isb-ml95d629.beaverton.k12.or.us>
2022-10-24 14:18:51 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h3>ISB's Awesome And Totally Not Broken Chat App</h3>
+    </div>
+  )
+}
+
+export default App
